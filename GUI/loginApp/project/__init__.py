da04b9dfba469c993eca7af8e77f60f2b983e58e
Ian Renfro <ian@renfro.org>
2017-11-19 20:35:26 -0500

    Pushed fully working login flask+angularjs app

---
+++ GUI/loginApp/project/__init__.py
@@ -0,0 +1,60 @@
+# project/__init__.py
+
+
+from flask import Flask, request, jsonify, session
+from project.models import User
+
+
+# config
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello"
+
+# routes
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    json_data = request.json
+    user = User(
+        email=json_data['email'],
+        password=json_data['password']
+    )
+    # TODO Check to see if user is registered
+    return jsonify({'result': True})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    print("Logging in")
+    json_data = request.json
+    print(json_data)
+    #user = User.query.filter_by(email=json_data['email']).first()
+    session['logged_in'] = True
+    status = True
+    print("returning")
+    return jsonify({'result': status})
+
+
+@app.route('/api/logout')
+def logout():
+    session.pop('logged_in', None)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/api/status')
+def status():
+    if session.get('logged_in'):
+        if session['logged_in']:
+            return jsonify({'status': True})
+    else:
+        return jsonify({'status': False})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ GUI/loginApp/project/models.py
@@ -0,0 +1,28 @@
+# project/models.py
+
+
+import datetime
+
+
+class User:
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.email)
